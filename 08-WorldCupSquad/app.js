d951d6323005603d14545bb342186f89b8aeaf28
umairayub-dev <umairayub049@gmail.com>
2023-04-21 05:52:24 +0500

    FEAT:Add Tasks 8

---
+++ 08-WorldCupSquad/app.js
@@ -0,0 +1,6 @@
+var worldCupSquad = ["Babar Azam", "Fakhar Zaman", "Imam-ul-Haq", "Mohammad Rizwan", "Sarfaraz Ahmed", 
+                 "Shadab Khan", "Imad Wasim", "Shoaib Malik", "Asif Ali", "Hasan Ali", "Mohammad Amir", 
+                 "Shaheen Afridi", "Haris Rauf", "Mohammad Hasnain", "Usman Qadir"]
+
+var selectedPlayers = worldCupSquad.slice(0,11)
+console.log("Orignal Squad: \n" + worldCupSquad + "\n\nSelected Players: \n" + selectedPlayers)
